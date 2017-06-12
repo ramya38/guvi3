69973295715f0da854b1fb1df4983f08c453d9b9
ramya38 <sramya2309@gmail.com>
2017-06-12 15:34:24 +0530

    Create xyz.java

---
+++ xyz.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class xyz
+{
+	public static void main(String args[])
+	{
+		int i;
+		Scanner sc=new Scanner(System.in);
+		String n=sc.next();
+		for(i=0;i<5;i++)
+		{
+		System.out.println(n);
+		}
+	}
+}
